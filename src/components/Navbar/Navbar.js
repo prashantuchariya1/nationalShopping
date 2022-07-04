cad934d79678c41ef74d2cd76400c25929b97e9b
prashantuchariya1 <prashantuchariya1@gmail.com>
2022-07-04 20:14:44 +0530

    full responsive3

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import style from "./Navbar.module.css";
 import {FcSearch} from  "react-icons/fc";
 import {RiAdminLine} from "react-icons/ri";
@@ -13,7 +13,7 @@ export default function Navbar() {
       <div className={style.childNavbar}>
         <div className={style.logo}>National Shopping</div>
         <div className={style.navMenu}>
-          <div className={style.menuItem}><a href="#" className={style.menuItemAnchor}>Home1</a> </div>
+          <div className={style.menuItem}><a href="#" className={style.menuItemAnchor}>Home</a> </div>
           <div className={style.menuItem}><a href="#" className={style.menuItemAnchor}>Mens</a></div>
           <div className={style.menuItem}><a href="#" className={style.menuItemAnchor}>Womens</a></div>
           <div className={style.menuItem}><a href="#" className={style.menuItemAnchor}>Baby Collection</a></div>
